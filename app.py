ea8259895fdafc506a52061fe378a92d83a6fe5b
SasukeUchiha7 <apil07cr@gmail.com>
2021-06-02 09:11:12 +0530

    modified

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
-app = Flask(__name__)   
 
-@app.route('/')
+
+app = Flask(__name__)  
+
+
+
+
+def _loadSongs():
+    return 'songName'
+
+# routes -------------
+
+@app.route('/', methods = ["POST","GET"])
 def home():
+    if request.method =="POST":
+        pass
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# functions-----------------
+
+def separateAudio():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True) 
