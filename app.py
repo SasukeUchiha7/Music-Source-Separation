2bddeb9ebc026070ce1ae851c5db5d14023db435
SasukeUchiha7 <apil07cr@gmail.com>
2021-06-02 11:25:30 +0530

    added files

--- app.py
+++ app.py
@@ -1,28 +1,60 @@
-from flask import Flask, render_template, url_for, request
+import os
+from flask import Flask, render_template, url_for, request, flash, redirect, send_from_directory
+from werkzeug.utils import secure_filename
 
-
+UPLOAD_FOLDER= "static/uploads"
+ALLOWED_EXT = { 'wav', 'aac'}
 
 app = Flask(__name__)  
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+app.add_url_rule(
+    "/uploads/<name>", endpoint="download_file", build_only=True
+)
 
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXT
 
-def _loadSongs():
-    return 'songName'
 
 # routes -------------
-
-@app.route('/', methods = ["POST","GET"])
+@app.route('/')
 def home():
+    return render_template('index.html', songs=[])
+
+@app.route('/upload',methods = ["POST","GET"])
+def upload():
+    # if request.method == 'POST':
+    #   f = request.files['file']
+    #   f.save(secure_filename(f.filename))
+    #   return 'file uploaded successfully'
     if request.method =="POST":
-        pass
-    return render_template('index.html')
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('download_file', name=filename))
+            source = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            return render_template('index.html', source = source)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# functions-----------------
+# functions -----------------
+
+def loadUploadFile():
+    return ''''''
 
 def separateAudio():
     pass
--- separation.py
+++ separation.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import torch
 import torchaudio
 import numpy as np
@@ -9,11 +8,12 @@ import youtube_dl
 from scipy.io.wavfile import read, write
 import wave
 import soundfile as sf
+import audiofile as af
 from openunmix import predict
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
-data,fs = sf.read ('test.wav')
+data, fs = sf.read('test4.wav', dtype='float32')
 
 estimates = predict.separate(
     torch.as_tensor(data).float(),
@@ -23,31 +23,4 @@ estimates = predict.separate(
 for target, estimate in estimates.items():
     print(target)
     audio = estimate.detach().cpu().numpy()[0]
-    write(target+'.wav',fs,data)
-=======
-import torch
-import torchaudio
-import numpy as np
-import scipy 
-# import stempeg
-import os
-import youtube_dl
-from scipy.io.wavfile import read, write
-import wave
-import soundfile as sf
-from openunmix import predict
-
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
-data,fs = sf.read ('test.wav')
-
-estimates = predict.separate(
-    torch.as_tensor(data).float(),
-    rate=fs,
-    device=device
-)   
-for target, estimate in estimates.items():
-    print(target)
-    audio = estimate.detach().cpu().numpy()[0]
-    write(target+'.wav',fs,data)
->>>>>>> initial commit
+    af.write('static/sounds/'+target+'.wav',audio,fs)
