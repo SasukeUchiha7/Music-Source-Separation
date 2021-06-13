bf15c23eec830a9efb0c917d68a6d05ff39900ed
SasukeUchiha7 <apil07cr@gmail.com>
2021-06-14 00:08:26 +0530

    fixed(2)

--- app.py
+++ app.py
@@ -35,69 +35,75 @@ def home():
     global mainSource
     mainSource=''
     delete_all()
-    return render_template('index.html', songs=songs)
+    return render_template('index.html', songs=songs )
 
 @app.route('/uploads/<name>') ##uploaded_file uri---------------------------
 def download_file(name):
     return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+
+@app.route('/<src>')
+def loaded(src):
+    audiosrc = src
+    mainSource = audiosrc
+    print(src)
+    return render_template('index.html', source=src)
 
 @app.route('/upload',methods = ["POST","GET"]) #### upload-------------------------------
 def upload(): 
     if request.method =="POST":
         if 'file' not in request.files:
             flash('No file part','danger')
-            return render_template('index.html', songs=songs)
+            return redirect(url_for('home'))
         file = request.files['file']
         print("uploaded file: ",file)
         if file.filename == '':
-            flash('No selected file','danger')
-            return render_template('index.html', songs=songs)
+            flash('File not selected!!','danger')
+            return redirect(url_for('home'))
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             # return redirect(url_for('download_file', name=filename))
-            source = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            source =os.path.normpath(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             global mainSource
             mainSource=''+source
             print("uploaded mainsrc:",mainSource)
             flash("Loaded...",'info')
-            return render_template('index.html', source = source)
+            return redirect(url_for('loaded', src = source))
         else: 
             flash("Wrong Format",'danger')
-            return render_template('index.html', songs=songs)
-
+            return redirect(url_for('home'))
 
 @app.route('/select', methods=['GET', 'POST'])  ## route for sample select
 def selected():
     selectedSource = request.args.get('select-song')
     if (selectedSource==None):
         flash("Please select from the given samples or upload a wav file!",'warning')
-        return render_template('index.html', songs=songs)
+        return redirect(url_for('home'))
     print("selected song:",selectedSource)
-    src=os.path.join(app.config['SAMPLE_FOLDER'],selectedSource)
+    src=os.path.normpath(os.path.join(app.config['SAMPLE_FOLDER'],selectedSource))
     global mainSource
     mainSource =''+src
     print("selected mainsrc:",mainSource)
     flash("Loaded...",'info')
-    return render_template('index.html', source = src)
+    return redirect(url_for('loaded', src = src))
 
 
-@app.route('/predict', methods=['POST','GET']) ## separate -------------------------
+@app.route('/process') ## separate -------------------------
 def separateAudio():
     print("separating mainsrc:",mainSource)
     if(mainSource==''):
         print("empty----------------------------------------------")
         flash("Empty File!!!!!",'danger')
-        return render_template('index.html', songs=songs)
+        return redirect(url_for('home'))
     else:
         separateMusic(mainSource)
         audios = os.listdir(app.config['OUTPUT_FOLDER'])
+        print(audios)
         output = []
         for a in audios: output.append(app.config['OUTPUT_FOLDER']+'/'+a)
         flash('Successfull!', 'success')
         return render_template('index.html', audios=output, source = mainSource)
 
-    
 
 @app.route('/about') ### about page---------------------------------
 def about():
@@ -114,4 +120,4 @@ def delete_all():
 
 
 if __name__ == '__main__':
-    app.run(threaded=True,port=5000) 
+    app.run(debug=True,port=5000) 
